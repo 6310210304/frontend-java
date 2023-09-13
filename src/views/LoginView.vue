f2bf4e3d7cd0cf8663e581c3cb1a091baf24b2d8
6310210304 <6310210304@psu.ac.th>
2023-09-13 11:25:43 +0700

    Lab8

---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- <v-card>
+        <v-container> -->
+            <!-- <v-row>
+                <v-col cols="1" v-for="i in 12" :key="i">
+                    {{ 'col -' + i }}
+                </v-col>
+            </v-row> -->
+            <v-row>
+                <v-col cols="12">
+                    <LoginForm/>
+                </v-col>
+            </v-row>
+        <!-- </v-container>
+    </v-card> -->
+</template>
+
+<script>
+import LoginForm from '../components/Login.vue'
+export default {
+    components: {
+        LoginForm
+    }
+}
+</script>
+
+<style>
+
+</style>
